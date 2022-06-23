5276ebec463681090853022b7634f16d9375daeb
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-06-23 17:22:00 -0600

    commit tree f3924526289555636d4681ef996ae2036d50d523

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push('Ralph')
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+function appendCat(name) {
+    return [...cats, "Broom"]
+}
+
+function prependCat(name) {
+    return ["Arnold", ...cats]
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length -1)
+}
